38abc8b65f61c478df76903c6a71123e10f7898e
kassieRepo2 <186893026+Kjw072892@users.noreply.github.com>
2026-04-06 17:54:05 -0700

    added sad test to math

--- tests/v1/math.test.ts
+++ tests/v1/math.test.ts
@@ -9,3 +9,10 @@ describe('GET /v1/math/add?q=10%2B10', () => {
     expect(res.body.result).toBe('20');
   });
 });
+
+describe('GET /v1/math/add?q=10', () => {
+  it('Adds the two numbers between the unicode for + in the URL', async () => {
+    const res = await request(app).get('/v1/math/add?q=10');
+    expect(res.status).toBe(400);
+  });
+});
